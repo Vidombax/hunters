409839b9f7c7d6db9bd8b1d1411fc9c8494d59ac
Vidombax <kirilka360@gmail.com>
2025-12-09 15:36:36 +0500

    стили для страницы авторизации

--- frontend/src/pages/Login.vue
+++ frontend/src/pages/Login.vue
@@ -2,18 +2,24 @@
   import { ref } from 'vue'
 
   const isLoginModal = ref(true);
+
+  const handlerChooseModal = () => {
+    isLoginModal.value = isLoginModal.value !== true;
+  }
 </script>
 
 <template>
   <div class="login" v-if="isLoginModal">
-    <input type="text" placeholder="Логин">
-    <input type="password" placeholder="Пароль">
+    <input type="text" placeholder="Логин" />
+    <input type="password" placeholder="Пароль" />
     <button>Войти</button>
+    <p class="choose_modal" @click="handlerChooseModal">Нету аккаунта</p>
   </div>
   <div class="registration" v-else>
-    <input type="text" placeholder="Придумайте логин">
-    <input type="password" placeholder="Придумайте пароль">
-    <button>Зарегистрироваться</button>
+    <input type="text" placeholder="Придумайте логин" />
+    <input type="password" placeholder="Придумайте пароль" />
+    <button style="width: 225px;">Зарегистрироваться</button>
+    <p class="choose_modal" @click="handlerChooseModal">Войти в существующий аккаунт</p>
   </div>
 </template>
 
@@ -24,5 +30,22 @@
     justify-content: center;
     align-items: center;
     flex-direction: column;
+    gap: 12px;
+  }
+  .login input,
+  .registration input {
+    width: 15%;
+  }
+  .choose_modal {
+    color: gray;
+    text-decoration: underline;
+    cursor: pointer;
+  }
+
+  @media screen and (max-width: 1050px) {
+    .login input,
+    .registration input {
+      width: 60%;
+    }
   }
 </style>
