0448fda8d4b9823fee18e8b5989f34c5eccb9a12
vidombax <kirilka360@gmail.com>
2025-12-13 13:26:12 +0500

    начал делать апвоуты

--- frontend/src/components/Votes.vue
+++ frontend/src/components/Votes.vue
@@ -1,18 +1,47 @@
 <script setup>
+  import { ref } from 'vue'
   import Arrow from '@/components/svg/Arrow.vue'
+  import { useUserStore } from '@/stores/user_store.js'
 
   const props = defineProps({
     votes: Number
-  })
+  });
+  const votes = ref(props.votes);
+  const userStore = useUserStore();
+
+  const voteHandler = async (typeVote, voteFunc) => {
+    try {
+      switch (voteFunc) {
+        case "thread":
+          if (typeVote === true) {
+            votes.value += 1;
+          }
+          else {
+            votes.value -= 1;
+          }
+          break;
+        case "comment":
+          if (typeVote === true) {
+
+          }
+          else {
+
+          }
+      }
+    }
+    catch (e) {
+      console.error(e);
+    }
+  }
 </script>
 
 <template>
   <div class="votes">
-    <div class="up"><Arrow/></div>
-    <div v-if="votes > 0" class="positive">{{ props.votes }}</div>
-    <div v-else-if="votes === 0" class="passive">{{ props.votes }}</div>
-    <div v-else class="negative">{{ props.votes }}</div>
-    <div class="down"><Arrow /></div>
+    <div class="up"><Arrow @click="voteHandler(true, 'thread')" /></div>
+    <div v-if="votes > 0" class="positive">{{ votes }}</div>
+    <div v-else-if="votes === 0" class="passive">{{ votes }}</div>
+    <div v-else class="negative">{{ votes }}</div>
+    <div class="down"><Arrow @click="voteHandler(false, 'thread')" /></div>
   </div>
 </template>
 
